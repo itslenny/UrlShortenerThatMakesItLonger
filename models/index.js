24f38f7a31337aaf6399176de38e45a70ea8c682
Lenny Urbanowski <lenny@itslennysfault.com>
2014-12-04 20:52:38 -0800

    added heroku db code

--- models/index.js
+++ models/index.js
@@ -5,6 +5,16 @@ var path      = require("path");
 var Sequelize = require("sequelize");
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
+
+/// NEEDED FOR HEROKU ///////////
+if(config.use_env_variable){
+  var db_info = process.env[value].match(/([^:]+):\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+  config.username=db_info[0];
+  config.password=db_info[1];
+  config.database=db_info[2];  
+}
+////////////////////////////////
+
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
 
