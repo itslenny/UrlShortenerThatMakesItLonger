c71bf608f46de5d36fd8fccda69106f6d6d76aa7
Lenny Urbanowski <lenny@itslennysfault.com>
2014-12-08 16:28:01 -0800

    added a comment

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var models = require("./models");
 var bodyParser = require('body-parser');
 
+// THIS IS A NEW COMMENT
+
 app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended:false}));
